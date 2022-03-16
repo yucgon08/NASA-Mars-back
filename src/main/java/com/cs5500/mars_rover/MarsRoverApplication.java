4834d463359abccdc8f61792e3596f38be975921
EthanLin <gyouza_azuoyg@outlook.com>
2022-03-16 01:36:11 -0700

    Spring Boot initialization.

---
+++ src/main/java/com/cs5500/mars_rover/MarsRoverApplication.java
@@ -0,0 +1,13 @@
+package com.cs5500.mars_rover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs5500/mars_rover/MarsRoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs5500.mars_rover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
